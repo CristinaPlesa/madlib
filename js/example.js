f41261fda35811e43b927e839f778e76f14f44fa
Cristina Plesa <cristinaplesa8@gmail.com>
2021-03-29 14:08:24 -0700

    Add array loop to js function.
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

---
+++ js/example.js
@@ -0,0 +1,9 @@
+let kittens = [1,2,3,4,5,6,7]
+
+kittens.forEach(function (kitten) {
+console.log(kitten)
+})
+
+function coolFunction() {
+  
+}
--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,18 @@ $(document).ready(function() {
     const exclamationinput = $("input#exclamation").val();
     const verbinput = $("input#verb").val();
     const nouninput = $("input#noun").val();
+    let userInputs = [person1input, person2input, animalinput, exclamationinput, verbinput, nouninput]
+    userInputs.forEach(function(userInput) {
+      $(".person1").text(userInputs[0]);
+      $(".person2").text(userInputs[1]);
+      $(".animal").text(userInputs[2]);
+      $(".exclamation").text(userInputs[3]);
+      $(".verb").text(userInputs[4]);
+      $(".noun").text(userInputs[5]);
+      
+      $("#story").show();
+      });
 
-    $(".person1").text(person1input);
-    $(".person2").text(person2input);
-    $(".animal").text(animalinput)
-    $(".exclamation").text(exclamationinput);
-    $(".verb").text(verbinput);
-    $(".noun").text(nouninput);
-    
-    $("#story").show();
     event.preventDefault();
-  })
-})
+  });
+});
